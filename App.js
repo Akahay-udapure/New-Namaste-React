a393f387fa8a09032fc71837a1eb8a143cee1527
Akahay-udapure <78835239+Akahay-udapure@users.noreply.github.com>
2023-10-07 15:03:13 +0530

    Episode 3

--- App.js
+++ App.js
@@ -1,29 +1,44 @@
-/**
- * <div id="parent">
-        <div id="child">
-            <h1>i am an h1 tag</h1>
-            <h2>i am an h2 tag</h2>
-        </div>
-        <div id="child2">
-            <h1>i am an h1 tag</h1>
-            <h2>i am an h2 tag</h2>
-        </div>
- * </div>
- */
-
 import React from "react";
-import  ReactDOM  from "react-dom/client";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "i am an h1 tag"),
-    React.createElement("h2", {}, "i am an h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "i am an h1 tag"),
-    React.createElement("h2", {}, "i am an h2 tag"),
-  ]),
+const parent = React.createElement("div", { id: "core-react-parent" }, [
+    React.createElement("div", { id: "child" }, [
+        React.createElement("h1", {}, "React core h1 tag"),
+        React.createElement("h2", {}, "React core h2 tag"),
+    ]),
+    React.createElement("div", { id: "child2" }, [
+        React.createElement("h1", {}, "React core h1 tag"),
+        React.createElement("h2", {}, "React core h2 tag"),
+    ]),
 ]);
 
+const Title = (
+    <div id="JSX-element-parent">
+      <div id="JSX-element-child">
+        <h1>JSX Element h1</h1>
+        <h2>JSX Element h2</h2>
+      </div>
+      <div id="JSX-element-child2">
+        <h1>JSX Element h1</h1>
+        <h2>JSX Element h2</h2>
+      </div>
+    </div>
+);
+
+const HeadingComponent = () => (
+    <div id="JSX-functionlComponent-parent">
+        <span id="add-react-core-element">{parent}</span><br />
+        <span id="add-jsx-element">{Title}</span>
+        <div id="JSX-functionlCommponent-child">
+            <h1>JSX Functional Component Child H1</h1>
+            <h2>JSX Functional Component Child H1</h2>
+        </div>
+        <div id="JSX-functionlCommponent-child2">
+            <h1>JSX Functional Component Child H1</h1>
+            <h2>JSX Functional Component Child H1</h2>
+        </div>
+    </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent />);
