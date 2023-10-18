87b1e9dbf63d22fe4848fe635432c0074018d542
Akahay-udapure <78835239+Akahay-udapure@users.noreply.github.com>
2023-10-18 12:22:30 +0530

    Episode 6

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,29 +1,79 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { resList } from "../utils/mockData";
 import RestaurantCard from "./RestaurantCard";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-    const [listOfRestaurants, setListOfRestaurants] = useState(resList);
-    return (
+    const [listOfRestaurants, setListOfRestaurants] = useState([]);
+
+    const [filteredList, setFilteredList] = useState([]);
+
+    const [searchText, setSearchText] = useState("");
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        const data = await fetch(
+            "https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.145923&lng=79.08762999999999&collection=96594&tags=layout_ux4&sortBy=&filters=&type=rcv2&offset=0&page_type=null",
+        );
+        const json = await data.json();
+        const restaurantList = json?.data?.cards?.filter(
+            (res) =>
+                res.card.card["@type"] ==
+                "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+        );
+        setListOfRestaurants(restaurantList);
+        setFilteredList(restaurantList);
+    };
+
+    return listOfRestaurants.length === 0 ? (
+        <Shimmer />
+    ) : (
         <div className="body">
-            <div className="search">
+            <div className="filter">
                 {/* Search */}
-                <button
-                    className="filter-btn"
-                    onClick={() => {
-                        const filteredRes = listOfRestaurants.filter(
-                            (res) => res.data.avgRating >= 4,
-                        );
-                        setListOfRestaurants(filteredRes);
-                    }}>
-                    Top Rated Restaurants
-                </button>
+                <div className="search">
+                    <input
+                        type="text"
+                        className="search-box"
+                        value={searchText}
+                        onChange={(e) => {
+                            setSearchText(e.target.value);
+                        }}
+                    />
+                    <button
+                        className="filter-btn"
+                        onClick={() => {
+                            let filterRes = listOfRestaurants.filter((res) =>
+                                (res?.card?.card?.info?.name)
+                                    .toLowerCase()
+                                    .includes(searchText.toLowerCase()),
+                            );
+                            setFilteredList(filterRes);
+                        }}>
+                        Search
+                    </button>
+                </div>
+                <div className="top-res">
+                    <button
+                        className="filter-btn"
+                        onClick={() => {
+                            const filteredRes = listOfRestaurants.filter(
+                                (res) => res?.card?.card?.info.avgRating >= 4,
+                            );
+                            setFilteredList(filteredRes);
+                        }}>
+                        Top Rated Restaurants
+                    </button>
+                </div>
             </div>
             <div className="res-container">
-                {listOfRestaurants.map((restaurant) => (
+                {filteredList.map((restaurant) => (
                     <RestaurantCard
-                        key={restaurant.data.id}
-                        resData={restaurant}
+                        key={restaurant?.card?.card?.info?.id}
+                        resData={restaurant?.card?.card?.info}
                     />
                 ))}
             </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,16 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 
 const Header = () => {
+    //In this code the question is how can const variable is getting updated?
+    // beacuse when first time component get render its new variiable with  value login as soon as setBtnNameReact gets called component will get re-renderd and btnNameReact is a new variable with the value Logout
+    const [btnNameReact, setBtnNameReact] = useState("Login");
+    // let btnName = "Login"; // this scenario not work thats why useState comes into picture
+    console.log("header render");
     return (
         <div className="header">
             <div className="logo-container">
-                <img
-                    className="logo"
-                    src={LOGO_URL}
-                />
+                <img className="logo" src={LOGO_URL} />
             </div>
             <div className="nav-items">
                 <ul>
@@ -15,6 +18,17 @@ const Header = () => {
                     <li>About Us</li>
                     <li>Contact Us</li>
                     <li>Cart</li>
+                    <li>
+                        <button
+                            onClick={() => {
+                                // btnName = "Logout" //this scenario not work thats why useState comes into picture
+                                btnNameReact == "Login"
+                                    ? setBtnNameReact("Logout")
+                                    : setBtnNameReact("Login");
+                            }}>
+                            {btnNameReact}
+                        </button>
+                    </li>
                 </ul>
             </div>
         </div>
